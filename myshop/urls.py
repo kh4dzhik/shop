cb2b953e2f00cb6243d46604bf35810c654cd09c
Your Name <you@example.com>
2024-01-07 21:31:36 +0300

    first commit

---
+++ myshop/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from myshop import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
+    path('payment/', include('payment.urls', namespace='payment')),
+    path('coupons/', include('coupons.urls', namespace='coupons')),
+    path('', include('shop.urls', namespace='shop')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
